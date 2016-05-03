3b1875dacd72aa3e55455863e5cda13d1469a4dd
ahaddjeri1@gmail.com <ahaddjeri1@gmail.com>
2016-05-03 17:31:57 +0200

    try

---
+++ recuit_simule.cpp
@@ -0,0 +1,18 @@
+// Optimisation de la methode avec le recuit simulé
+#include<iostream>
+#include <cmath> //sqrt
+#include <cstdlib> //rand
+#include <ctime> //intialisatn de rand
+#include <string>
+using namespace std;
+double proba0_1()//loi de proba entre 1 et 0 fonctionnelle
+{
+  return (double)rand()%(RAND_MAX); //valur aleatoire du nombre entre 0 et 100
+}
+
+
+
+int main(){
+string test ="bonjour";
+cout<<test;
+return 0;}
